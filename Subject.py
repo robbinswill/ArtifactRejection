6aca5bb67fba1ce73738ec2a6f349a8c9a066dd5
wrobbins <will.robbins@dal.ca>
2021-06-17 09:24:39 -0300

    Fixed Subject class __init__ method

--- Subject.py
+++ Subject.py
@@ -5,7 +5,7 @@ class Subject:
     Subject class for reading EEG data and performing pre-processing
     """
 
-    def _init__(self, name):
+    def __init__(self, name):
         self.subject_name = name
 
     def read_data(self):
