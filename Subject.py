c70bf84daf3af1463fca2d55a21d0e27da72e843
wrobbins <will.robbins@dal.ca>
2021-06-17 09:23:40 -0300

    Started Subject.py

---
+++ Subject.py
@@ -0,0 +1,12 @@
+
+
+class Subject:
+    """
+    Subject class for reading EEG data and performing pre-processing
+    """
+
+    def _init__(self, name):
+        self.subject_name = name
+
+    def read_data(self):
+        pass
