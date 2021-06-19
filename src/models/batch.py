5937ade1dfff17e1d1df0d9c1550ca5cbfb6daee
wrobbins <will.robbins@dal.ca>
2021-06-19 08:54:07 -0300

    Added read_in() and generate_subjects() methods to batch.py

--- src/models/batch.py
+++ src/models/batch.py
@@ -5,5 +5,23 @@ sys.path.append('../src')
 from src.config import config
 from src.dataloader import subject
 
-# Get the list of subjects
-subjects = config.get_subject_names()
+
+def read_in():
+    """
+    Read-in the list of subjects from the CSV in the data folder
+    :return: a list of subject names
+    """
+    return config.get_subject_names()
+
+
+def generate_subjects():
+    """
+    Generate a Subject object for each read-in subject
+    :return: a dict of Subjects in the form { Subject name: Subject object}
+    """
+    names = read_in()
+    subjects = {}
+    for n in names:
+        subjects[n] = subject.Subject(n)
+
+    return subjects
