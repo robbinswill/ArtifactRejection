bee709633030ba8f571dc170469b3a16626a2768
wrobbins <will.robbins@dal.ca>
2021-08-03 14:50:20 -0300

    Added BIDS generate method to main.py

--- main.py
+++ main.py
@@ -2,13 +2,17 @@
 Main driver script
 """
 from src.models import batch
+from src.utils import generate_data
 
 
 def run():
     print('Executing main.py...')
-    test = batch.SubjectBatch()
-    test.generate_subjects()
-    test.test()
+    print('Generating BIDS...')
+    generate_data.generate_BIDS()
+    print('Running preprocessing pipeline...')
+    # test = batch.SubjectBatch()
+    # test.generate_subjects()
+    # test.test()
     # test.execute_serial()
     # test.execute_preprocessing()
     print('main.py finished')
